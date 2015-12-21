9f9637f92a8944f9e3d626981311e85a689ddc2b
RasAlhague <alexgr.dev@gmail.com>
2015-12-21 18:51:59 +0200

    URI extractor

--- user/user_main.c
+++ user/user_main.c
@@ -4,6 +4,8 @@
 #include <gpio.h>
 #include "osapi.h"
 
+#define DLEVEL 2
+
 void init_gpios(void) {
     os_printf("Initing GPIO to LOW\n");
 
@@ -17,25 +19,54 @@ void connect_to_wifi(void) {
 
     struct station_config stationConf;
 
+#if DLEVEL > 2
     os_printf("wifi_set_opmode(STATION_MODE)\n");
+#endif
     wifi_set_opmode(STATION_MODE);
 
     os_memcpy(&stationConf.ssid, ssid, 32);
     os_memcpy(&stationConf.password, password, 32);
 
     wifi_station_set_config(&stationConf);
+#if DLEVEL > 2
     os_printf("wifi_station_connect()\n");
+#endif
     wifi_station_connect();
 }
 
 void http_disconnetcb(void *arg) {
     struct espconn *pespconn = arg;
 
+#if DLEVEL > 2
     os_printf("Disconnecting \n");
-//    os_printf(pespconn->proto.tcp->remote_ip);
-//    os_printf("\n");
+#endif
 
     espconn_disconnect(pespconn);
+}
+
+void extract_uri(char *__dest, char *__response) {
+    char uri[64];
+
+    char *http_str_poiner;
+    http_str_poiner = strstr(__response, " HTTP");
+#if DLEVEL > 2
+    os_printf("Pointer to HTTP str = %s\n\n", http_str_poiner);
+#endif
+
+    char *slash_str_poiner;
+    slash_str_poiner = strstr(__response, "/");
+#if DLEVEL > 2
+    os_printf("Pointer to / str = %s\n", slash_str_poiner);
+#endif
+
+    size_t p_url_end = (size_t) (http_str_poiner - slash_str_poiner);
+    strncpy(uri, slash_str_poiner, p_url_end);
+    uri[p_url_end] = '\0';
+#if DLEVEL > 1
+    os_printf("uri = %s\n", uri);
+#endif
+
+    strcpy(__dest, uri);
 }
 
 void http_recvcb(void *arg,
@@ -47,20 +78,25 @@ void http_recvcb(void *arg,
 
     os_memcpy(response, pdata, len);
 
+    extract_uri(uri, response);
+#if DLEVEL > 2
+    os_printf("uri = %s\n", uri);
+    os_printf("%d\n", strcmp(uri, "/gpio0/low") == 0);
+#endif
 
-    char *http_str_poiner;
-    http_str_poiner = strstr(response, " HTTP");
-//    os_printf("Pointer to HTTP str = %s\n\n", http_str_poiner);
-
-    char *slash_str_poiner;
-    slash_str_poiner = strstr(response, "/");
-//    os_printf("Pointer to / str = %s\n", slash_str_poiner);
-
-    size_t p_url_end = (size_t) (http_str_poiner - slash_str_poiner);
-    strncpy(uri, slash_str_poiner, p_url_end);
-    uri[p_url_end] = '\0';
-//    os_printf("uri = %s\n", uri);
-
+    if (strcmp(uri, "/gpio0/low") == 0) {
+        os_printf("/gpio0/low\n");
+        gpio_output_set(0, BIT0, BIT0, 0);
+    } else if (strcmp(uri, "/gpio0/high") == 0) {
+        os_printf("/gpio0/high\n");
+        gpio_output_set(BIT0, 0, BIT0, 0);
+    } else if (strcmp(uri, "/gpio1/low") == 0) {
+        os_printf("/gpio1/low\n");
+        gpio_output_set(0, BIT2, BIT2, 0);
+    } else if (strcmp(uri, "/gpio1/high") == 0) {
+        os_printf("/gpio1/high\n");
+        gpio_output_set(BIT2, 0, BIT2, 0);
+    }
 
     espconn_regist_sentcb(pespconn, http_disconnetcb);
     espconn_send(pespconn, uri, strlen(uri));
@@ -70,7 +106,9 @@ void http_recvcb(void *arg,
 void ICACHE_FLASH_ATTR server_connectcb(void *arg) {
     struct espconn *pespconn = (struct espconn *) arg;
 
+#if DLEVEL > 2
     os_printf("server_connectcb\n");
+#endif
 
     //Let's register a few callbacks, for when data is received or a disconnect happens.
     espconn_regist_recvcb(pespconn, http_recvcb);
@@ -95,7 +133,9 @@ void start_http_server(void) {
     espconn_regist_connectcb(pHTTPServer, server_connectcb);
 
     //Start listening!
+#if DLEVEL > 2
     os_printf("Start listening\n");
+#endif
     espconn_accept(pHTTPServer);
 
     //I don't know what default is, but I always set this.
@@ -105,7 +145,7 @@ void start_http_server(void) {
 void user_init(void) {
     uart_div_modify(0, UART_CLK_FREQ / 115200);
 
-    os_printf("SDK version:%s\n", system_get_sdk_version());
+    os_printf("SDK version: %s\n", system_get_sdk_version());
 
     init_gpios();
     connect_to_wifi();
